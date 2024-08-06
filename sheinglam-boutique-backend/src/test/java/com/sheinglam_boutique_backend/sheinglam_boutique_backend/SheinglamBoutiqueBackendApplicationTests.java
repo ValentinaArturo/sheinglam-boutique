bf0deb110648494687f2ef7dc26130ef86df430d
ValentinaArturo <74568726+ValentinaArturo@users.noreply.github.com>
2024-08-06 14:41:35 -0600

    Subir backend base

---
+++ sheinglam-boutique-backend/src/test/java/com/sheinglam_boutique_backend/sheinglam_boutique_backend/SheinglamBoutiqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheinglam_boutique_backend.sheinglam_boutique_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheinglamBoutiqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
