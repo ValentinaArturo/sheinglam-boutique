9bd50b29e20367c7d1a99188c2696ba5423c6342
ValentinaArturo <74568726+ValentinaArturo@users.noreply.github.com>
2024-10-24 11:46:56 -0600

    Agregar imports

--- Ecommerce-Admin-Panel-master/lib/screens/shipment/bloc/shipment_bloc.dart
+++ Ecommerce-Admin-Panel-master/lib/screens/shipment/bloc/shipment_bloc.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:ecommerce_admin_panel/common/bloc/base_state.dart';
+import 'package:ecommerce_admin_panel/screens/shipment/model/shipment_list_model.dart';
 import 'package:equatable/equatable.dart';
+
+import '../model/detalle_factura_list_model.dart';
+import '../model/factura_list_model.dart';
 
 part 'shipment_event.dart';
 part 'shipment_state.dart';
